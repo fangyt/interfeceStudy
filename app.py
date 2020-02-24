5c3ec05b1d1d7117806986a8d61c20dde70df9be
Fang Yong_Teng <fangyt0810@163.com>
2020-02-25 00:05:50 +0800

    fix:开放端口

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='47.107.54.106')
+    app.run(host='0.0.0.0')
