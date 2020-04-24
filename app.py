af4aea64b18cc10e373aa21049e821ce4c24ad0c
fangyongteng <fangyongteng@supermonkey.com.cn>
2020-04-24 15:39:00 +0800

    fix:新增接口笔记

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/',methods=['POST', 'GET'])
+@app.route('/interface',methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    return render_template('interface.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
